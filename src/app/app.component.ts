7607701c4b205f6747c645e3fa50ea4bf75faa28
prateekmahnot89 <mahnot.prateek@gmail.com>
2021-06-17 17:07:59 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  displayedColumns = ['id', 'first_name', 'last_name', 'email', 'gender'];
+
+  jsonData: any[] = [
+    {
+      id: 1,
+      first_name: 'Jeanette',
+      last_name: 'Penddreth',
+      email: 'jpenddreth0@census.gov',
+      gender: 'Female'
+    },
+    {
+      id: 2,
+      first_name: 'Giavani',
+      last_name: 'Frediani',
+      email: 'gfrediani1@senate.gov',
+      gender: 'Male'
+    },
+    {
+      id: 3,
+      first_name: 'Noell',
+      last_name: 'Bea',
+      email: 'nbea2@imageshack.us',
+      gender: 'Female'
+    },
+    {
+      id: 4,
+      first_name: 'Willard',
+      last_name: 'Valek',
+      email: 'wvalek3@vk.com',
+      gender: 'Male'
+    }
+  ];
+
+  dataSource = this.jsonData;
+}
+
+export interface jsonData {
+  id: number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
 }
